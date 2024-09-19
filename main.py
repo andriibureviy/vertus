aef06ae0bb16a2815ebb401ad2ce9a6845512cf0
andrii.yazvinskyi <andrii.yazvinskyi@lexima.tech>
2024-09-20 00:09:45 +0300

    init project

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import os
 
 async def main():
     print("Soft's author: https://t.me/ApeCryptor\n")
-    action = int(input("Select action:\n1. Start soft\n2. Get statistics\n3. Create sessions\n\n> "))
+    action = 1
 
     if not os.path.exists('sessions'): os.mkdir('sessions')
 
